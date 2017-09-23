8f5e800cedbe4af0baa858fa9fd4ce599b0e5439
Henri Mäkelä <hene.makela@gmail.com>
2017-09-24 01:22:51 +0300

    lisasin toimintoja suosikkeihin

--- app/src/main/java/com/mypackage/henri/ruokaa/DailyMenuActivity.java
+++ app/src/main/java/com/mypackage/henri/ruokaa/DailyMenuActivity.java
@@ -1,11 +1,20 @@
 package com.mypackage.henri.ruokaa;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
@@ -42,7 +51,69 @@ public class DailyMenuActivity extends AppCompatActivity {
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.list_item, foodList);
         dailyList.setAdapter(adapter);
 
+        //LISÄÄ RUOAN LEMPIRUOKIIN. JOKU SELKEÄMPI VAIHTOEHTO KUIN LONGCLICK?
+        dailyList.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                String ruoka = dailyList.getItemAtPosition(i).toString();
+                addToFavorites(ruoka);
+
+                return false;
+            }
+        });
+
 
 
     }
+
+
+
+
+    private void addToFavorites(String txt){
+
+        String FILENAME = "favorites";
+        String text = txt;
+
+        //JOS TIEDOSTO ON JO OLEMASSA NIIN LISÄTÄÄN RUOKA TIEDOSTOON.
+        //https://developer.android.com/reference/java/io/FileOutputStream.html
+        //NÄKÖJÄÄN VOIS TEHDÄ FILEWRITERILLÄ KOSKA DATA ON PELKKÄÄ TEKSTIMUOTOA.
+
+        if(fileExist(FILENAME)){
+            try {
+                FileOutputStream fileOutputStream = openFileOutput(FILENAME, Context.MODE_APPEND);
+
+                fileOutputStream.write(text.getBytes());
+                fileOutputStream.close();
+                Toast.makeText(this, "Lisätty suosikkeihin", Toast.LENGTH_SHORT).show();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        else{
+            // JOS TIEDOSTOA EI OLE NIIN LUODAAN SE. MODE_PRIVATE LUO TIEDOSTON AINOASTAAN SOVELLUKSEN KÄYTTÖÖN.
+            try {
+                FileOutputStream fileOutputStream = openFileOutput(FILENAME, Context.MODE_PRIVATE);
+
+                fileOutputStream.write(text.getBytes());
+                fileOutputStream.close();
+                Toast.makeText(this, "Lisätty suosikkeihin", Toast.LENGTH_SHORT).show();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+
+
+
+    }
+
+    //TARKISTETAAN ONKO TIEDOSTO OLEMASSA
+
+    public boolean fileExist(String fname){
+        File file = getBaseContext().getFileStreamPath(fname);
+        return file.exists();
+    }
+
+
 }
--- app/src/main/java/com/mypackage/henri/ruokaa/tab2fragment.java
+++ app/src/main/java/com/mypackage/henri/ruokaa/tab2fragment.java
@@ -1,5 +1,6 @@
 package com.mypackage.henri.ruokaa;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.constraint.ConstraintLayout;
@@ -7,10 +8,20 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 /**
  * Created by Henri on 20.9.2017.
@@ -20,28 +31,79 @@ public class tab2fragment extends Fragment {
     private static final String TAG = "tab2fragment";
 
     ImageButton addFavorite;
-
+    ArrayList<String> suosikkiRuoat;
+    ListView suosikkiRuoatList;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.tab2_fragment, container, false);
-        //VITUT TOIMI
-       /* addFavorite = (ImageButton)view.findViewById(R.id.lisaaBtn);
 
-        addFavorite.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                View thisLayout = (ConstraintLayout)view.findViewById(R.id.suosikit);
+        suosikkiRuoatList = (ListView)view.findViewById(R.id.list);
 
+        loadFavorites();
 
-                TextView text = new TextView(getView().getContext());
-                text.setText("NI5UU");
-                text.setLayoutParams(new ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.WRAP_CONTENT, ConstraintLayout.LayoutParams.WRAP_CONTENT));
-                ((ConstraintLayout) thisLayout).addView(text);
-            }
-        });
-        */
         return view;
     }
+
+    private String readFromFile(String filename){
+        //https://developer.android.com/reference/java/io/FilterInputStream.html
+        //TÄSSÄKIN TOIMISI FILEREADER KOSKA PELKKÄÄ TEKSTIÄ
+
+        String ruuat;
+        if(fileExist(filename)){
+            //JOS TIEDOSTO ON OLEMASSA NIIN TEKSTI LUETAAN SIITÄ. TÄMÄ PITÄÄ OLLA KOSKA MUUTEN OHJELMA KAATUU JOS SUOSIKKEJA EI OLE LISÄTTY
+            try {
+                FileInputStream fileInputStream = getActivity().openFileInput(filename);
+                //INPUTSTREAMREADER MUUTTAA BITIT TIEDOSTOSTA KIRJAIMIKSI
+                InputStreamReader inputStr =  new InputStreamReader(fileInputStream);
+                // BUFFEREDREADER LUKEE PUSKUROIMALLA KIRJAIMIA EDELTÄ JOTTA LUKEMINEN SUJUU SULAVASTI
+
+                BufferedReader bf = new BufferedReader(inputStr);
+
+                //STRINGBUFFER ON KUIN STRING MUTTA SIIHEN VOI SYÖTTÄÄ MITÄ TAHANSA DATATYYPPIÄ JA SE MUUTTAA SEN STRINGIKSI.
+                //SE EI MYÖSKÄÄN OLISI TÄSSÄ VÄLTTÄMÄTTÄ TARPEELLINEN
+                StringBuffer sb = new StringBuffer();
+
+                while((ruuat=bf.readLine()) != null){
+                    sb.append(ruuat + "\n");
+
+                }
+
+                return sb.toString();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+
+            }
+        }
+        // JOS TIEDOSTOA EI OLE NIIN PALAUTETAAN TYHJÄÄ
+        return "";
+    }
+
+    private void loadFavorites(){
+
+        // TUO SPLITTI EI TOIMI HYVIN. PITÄÄ SÄÄTÄÄ REGEXIT KUNTOON
+        suosikkiRuoat = new ArrayList<>();
+
+        String[] foodz = readFromFile("favorites").split("\n");
+
+        for(String r : foodz){
+            suosikkiRuoat.add(r);
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(),R.layout.list_item, suosikkiRuoat);
+        suosikkiRuoatList.setAdapter(adapter);
+
+    }
+
+
+
+
+    private boolean fileExist(String fname){
+        File file = getActivity().getFileStreamPath(fname);
+        return file.exists();
+    }
+
+
 }
